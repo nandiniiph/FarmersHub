3e9515bb52fdbdc31ebdcea927ef1a50a8c7d5bc
Nandini Putri <nandiniputrihanifa7@gmail.com>
2025-06-12 12:48:06 +0700

    hm

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ManajemenProdukController;
 use App\Http\Controllers\ManajemenAkunController;
-
 
 Route::get('/', function () {
     return view('LandingPage');
@@ -14,13 +13,20 @@ Route::get('/Login', [LoginController::class, 'showLogin'])->name('showLogin');
 Route::post('/proses-Login', [LoginController::class, 'login'])->name('login');
 Route::get('/Register', [LoginController::class, 'showRegister'])->name('showRegister');
 Route::post('/proses-Register', [LoginController::class, 'register'])->name('register');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 Route::get('/DashboardAdmin', [LoginController::class, 'showDashboardAdmin'])->name('showDashboardAdmin');
 Route::get('/DashboardKonsumen', [LoginController::class, 'showDashboardKonsumen'])->name('showDashboardKonsumen');
 Route::get('/DashboardPetani', [LoginController::class, 'showDashboardPetani'])->name('showDashboardPetani');
 
 Route::get('/Profile', [ManajemenAkunController::class, 'profile'])->middleware('auth')->name('profile.index');
 
-Route::resource('produk', ManajemenProdukController::class);
+Route::get('/produk/index', [ManajemenProdukController::class, 'index'])->name('produk.index');
+Route::get('/produk/tambahProduk', [ManajemenProdukController::class, 'create'])->name('produk.create');
+Route::post('/produk/simpanProduk', [ManajemenProdukController::class, 'store'])->name('produk.store');
+Route::get('/produk/editProduk/{id}', [ManajemenProdukController::class, 'edit'])->name('produk.edit');
+Route::put('/produk/updateProduk/{id}', [ManajemenProdukController::class, 'update'])->name('produk.update');
+Route::delete('/produk/hapusProduk/{id}', [ManajemenProdukController::class, 'destroy'])->name('produk.destroy');
 
 Route::get('/akun-petani', function () {
     return 'Halaman Akun Petani';
@@ -37,6 +43,3 @@ Route::get('/riwayat', function () {
 Route::get('/pesanan', function () {
     return 'Halaman Pesanan';
 })->name('pesanan.index');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-
